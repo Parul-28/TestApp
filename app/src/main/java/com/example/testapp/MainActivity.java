55991e519a7886aad27854ead22de065e117073c
Parul <parul.281997@gmail.com>
2019-03-10 13:49:20 +0530

    ok

---
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.testapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+
+        // Set a click listener on that View
+        button.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the family category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link FamilyActivity}
+                Intent intent = new Intent(MainActivity.this, secondActivity.class);
+
+                // Start the new activity
+                startActivity(intent);
+            }
+
+            });
+
+}}
---
+++ app/src/main/java/com/example/testapp/secondActivity.java
@@ -0,0 +1,13 @@
+package com.example.testapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class secondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+
+    }
+}
